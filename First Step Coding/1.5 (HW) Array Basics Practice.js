b4ee8eb13954d2ca17f38b74276da95182bfdbdc
dorson755 <68614535+dorson755@users.noreply.github.com>
2020-08-08 15:34:31 -0400

    Create 1.5 (HW) Array Basics Practice.js

---
+++ First Step Coding/1.5 (HW) Array Basics Practice.js
@@ -0,0 +1,27 @@
+/*
+ Challenge 1: Create an array containing the last
+ names of the first 5 US presidents in order, and 
+ assign it to a variable called "presidents". 
+ 
+ Make sure your spelling is correct, or the automated
+ tests will not run correctly.
+*/
+var presidents = ['Washington', 'Adams', 'Jefferson', 'Madison', 'Monroe']
+/*
+ Challenge 2: Create an array of the first 5
+ US states in alphabetical order, and assign it
+ to a variable called "states".
+ 
+ Using bracket syntax, add the sixth state by 
+ alphabetical order ('Colorado') to the end of this
+ array.
+*/
+var states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California']
+states[5] = 'Colorado'
+/*
+ Challenge 3: Log the length of each of your two 
+ arrays, in two separate statements.
+*/
+console.log = presidents.length
+
+console.log = states.length
