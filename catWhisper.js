a4fc722dcaa5345fbaa6ded118dc5095535a680b
dorson755 <68614535+dorson755@users.noreply.github.com>
2020-08-08 16:29:20 -0400

    Delete catWhisper.js

--- catWhisper.js
+++
@@ -1,7 +0,0 @@
-var cats = ['Sylvester', 'Tigger', 'Mr. Bigglesworth', 'Felix', 'Simba'];
-
-// @ts-ignore
-function forOneCat(cat){
-  console.log(cat.toLowerCase() + '...');
-}
-cats.forEach(forOneCat);
