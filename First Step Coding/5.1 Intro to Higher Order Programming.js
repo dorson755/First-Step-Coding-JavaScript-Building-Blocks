0a2378c5d07cd1f99b5ac1f6b519b330cb8d891a
dorson755 <68614535+dorson755@users.noreply.github.com>
2020-08-08 15:54:28 -0400

    Create 5.1 Intro to Higher Order Programming.js

---
+++ First Step Coding/5.1 Intro to Higher Order Programming.js
@@ -0,0 +1,11 @@
+var cats = ['Sylvester', 'Tigger', 'Mr. Bigglesworth', 'Felix', 'Simba'];
+
+
+var forOne = function(cat){
+
+  console.log(cat.toLowerCase() + '...');
+
+}
+
+
+cats.forEach(forOne)
